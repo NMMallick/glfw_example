e849ba6c104dc09997f12fe2f420845d9f4282c9
NMMallick <nmm109@pitt.edu>
2023-02-27 19:16:59 -0500

    testGLFW example

---
+++ build/CMakeFiles/some_random_binary.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nate/glfw_example/src/testGLFW.cpp" "/home/nate/glfw_example/build/CMakeFiles/some_random_binary.dir/src/testGLFW.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../external/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nate/glfw_example/build/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/testGLFW.h
@@ -0,0 +1,4 @@
+#ifndef TEST_GLFW_H_
+#define TEST_GLFW_H_
+
+#endif
